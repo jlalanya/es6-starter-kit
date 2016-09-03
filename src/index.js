d348e973150cf54b3c02b348dba247003d5414dc
Jose Alanya <jose@redisrupt.io>
2016-09-03 17:21:07 -0500

    This is my help

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import { project, showProject } from './projectView';
+import { initAdapter } from '../designpatterns/adapter/testTarget';
 
 project.projectId = 8000;
 
 showProject();
+initAdapter("todo", "12, 6");
+initAdapter("otherSetting", "13");
 
 console.log(project.projectId);
